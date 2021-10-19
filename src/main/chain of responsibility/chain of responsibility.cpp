d2ed2479a8db4110eaf290f0d013e7073a364e80
Dear_xxf <73422456+Dear-xxf@users.noreply.github.com>
2021-10-19 21:03:50 +0800

    责任链设计模式

---
+++ src/main/chain of responsibility/chain of responsibility.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+class Handler
+{
+public:
+    Handler(){NextHandler = NULL;}
+    virtual ~Handler(){}
+
+    void SetNextHandler(Handler *next){NextHandler = next;}
+
+    virtual void HandleRequest(int level){};
+protected:
+    Handler *NextHandler;
+};
+
+class InterimCommittee:public Handler
+{
+public:
+    virtual void HandleRequest(int level)
+    {
+        if(level <= 1)
+        {
+            cout<<"申诉已经交由临时委员会处理,请耐心等待！"<<endl;
+        }
+        else
+        {
+            NextHandler->HandleRequest(level);
+        }
+    }
+};
+
+class ExecutiveCommittee:public Handler
+{
+public:
+    virtual void HandleRequest(int level)
+    {
+        if(level <= 2)
+        {
+            cout<<"申诉已经交由奥委会秘书处处理,请耐心等待！"<<endl;
+        }
+        else
+        {
+            NextHandler->HandleRequest(level);
+        }
+    }
+};
+
+class PlenarySession:public Handler
+{
+public:
+    virtual void HandleRequest(int level)
+    {
+        if(level <= 3)
+        {
+            cout<<"申诉已经交由奥委会全会处理,请耐心等待！"<<endl;
+        }
+        else
+        {
+            cerr<<"权级有误！"<<endl;
+        }
+    }
+};
+
+int main()
+{
+    Handler *interimcommittee = new InterimCommittee;
+    Handler *executivecommittee = new ExecutiveCommittee;
+    Handler *plenarysession = new PlenarySession;
+
+    interimcommittee->SetNextHandler(executivecommittee);
+    executivecommittee->SetNextHandler(plenarysession);
+
+    interimcommittee->HandleRequest(1);
+    interimcommittee->HandleRequest(2);
+    interimcommittee->HandleRequest(3);
+    interimcommittee->HandleRequest(8);
+    system("pause");
+    return 0;
+}
