a259c628baeb8e02f13421795bac74fa6ca1af9b
yzh <65484058+yangzihao1952728@users.noreply.github.com>
2021-10-23 13:30:39 +0800

    Add files via upload

---
+++ src/main/Proxy/Proxy.cpp
@@ -0,0 +1,35 @@
+#include"Proxy.h"
+#include<iostream>
+using namespace std;
+/*
+	in this pattern,Olympic Drug Testing Center as the pointer, olympian as proxy
+*/
+OlympicDrugTest::OlympicDrugTest() {}
+OlympicDrugTest::~OlympicDrugTest() {}
+OlympicDrugTextResult::OlympicDrugTextResult() {}
+OlympicDrugTextResult::~OlympicDrugTextResult() {}
+void OlympicDrugTextResult::check()
+{
+	cout << "The drug test came back negative !" << endl;
+}
+Proxy::Proxy()
+{
+	result = new OlympicDrugTextResult();
+}
+Proxy::~Proxy()
+{
+	delete result;
+}
+void Proxy::check()
+{
+	cout << "Checking the results from the Olympic Drug Testing Center ..." << endl;
+	cout << "The result is :";
+	result->check();
+}
+int main()
+{
+	Proxy* proxy = new Proxy();
+	proxy->check();
+	delete proxy;
+	return 0;
+}
---
+++ src/main/Proxy/Proxy.h
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef _PROXY_H_
+#define _PROXY_H_
+//subject
+class OlympicDrugTest
+{
+public:
+	virtual ~OlympicDrugTest();
+	OlympicDrugTest();
+	virtual void check() = 0;
+};
+//real subject
+class OlympicDrugTextResult :public OlympicDrugTest
+{
+public:
+	OlympicDrugTextResult();
+	virtual ~OlympicDrugTextResult();
+	void check();
+};
+class Proxy :public OlympicDrugTest
+{
+public:
+	virtual ~Proxy();
+	Proxy();
+	void check();
+private:
+	OlympicDrugTextResult* result;
+};
+#endif // !_PROXY_H_
