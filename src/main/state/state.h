be7a14abd33e6e1738a4244a03e2e983011103f9
yzh <65484058+yangzihao1952728@users.noreply.github.com>
2021-10-23 13:28:59 +0800

    Add files via upload

---
+++ src/main/state/state.h
@@ -0,0 +1,57 @@
+#pragma once
+#ifndef _STATE_H_
+#define _STATE_H_
+class Context;
+class state
+{
+public:
+	state();
+	virtual ~state();
+	virtual void handle(Context* pContext) = 0;
+};
+class swimmingstate :public state
+{
+public:
+	swimmingstate();
+	~swimmingstate();
+	virtual void handle(Context* pContext);
+};
+class TransitionSectionA :public state
+{
+public:
+	TransitionSectionA();
+	~TransitionSectionA();
+	virtual void handle(Context* pContext);
+};
+class cyclingstate :public state
+{
+public:
+	cyclingstate();
+	~cyclingstate();
+	virtual void handle(Context* pContext);
+};
+class TransitionSectionB :public state
+{
+public:
+	TransitionSectionB();
+	~TransitionSectionB();
+	virtual void handle(Context* pContext);
+};
+class runningstate :public state
+{
+public:
+	runningstate();
+	~runningstate();
+	virtual void handle(Context* pContext);
+};
+class Context
+{
+public:
+	Context(state* pstate);
+	~Context();
+	void Request();
+	void Changestate(state* pstate);
+private:
+	state* _state;
+};
+#endif // !_STATE_H_
