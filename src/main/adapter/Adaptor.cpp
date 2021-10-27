fc4517ff3094e65cdd27e8e141f093e9832c95fb
zb2313 <57031778+zb2313@users.noreply.github.com>
2021-10-27 13:34:40 +0800

    Update Adaptor.cpp

--- src/main/adapter/Adaptor.cpp
+++ src/main/adapter/Adaptor.cpp
@@ -1,5 +1,18 @@
 //
 // Created by xzb23 on 2021/10/23.
 //
-
+#include<iostream>
+#include "ObjectAdaptor.h"
+using namespace std;
+void testAdaptor()
+{
+    Foreigner* t = new ForeignTranslator();
+    t->request();
+    delete t;
+}
+int main()
+{
+    testAdaptor();
+    return 0;
+}
 
