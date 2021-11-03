e668f8c57e10846fb8b63c2f89d6600a22e2a56a
Yvette2001 <87475993+Crestina2001@users.noreply.github.com>
2021-11-03 11:04:01 +0800

    strategy修改

--- src/main/strategy/strategy.h
+++ src/main/strategy/strategy.h
@@ -4,49 +4,95 @@
 #include<vector>
 using namespace std;
 struct Event {
-	string startTime;
-	string name;
+	string eventName;//比赛项目名
+	string time;//举办时间
+	string heldSite;//举办的场馆名
+	bool operator<(Event& e) {
+		return time < e.time;
+	}
 };
-bool sortByName(Event e1, Event e2) {
-	return e1.name < e2.name;
+struct Site {
+	string siteName;//场地名
+	int num;//场馆编号
+	vector<Event*>_events;
+};
+//按照名字排序
+bool sortByName(Site s1, Site s2) {
+	return s1.siteName < s2.siteName;
 }
-bool sortByTime(Event e1, Event e2) {
-	return e1.startTime < e2.startTime;
+//按照编号排序
+bool sortByNum(Site s1, Site s2) {
+	return s1.num < s2.num;
+}
+//按照项目名称排序
+bool sortByTime(Event* e1, Event* e2) {
+	return e1->time < e2->time;
 }
 
 class Compositor {
 
 public:
 	Compositor() {}
-	virtual void show(vector<Event>) = 0;
+	virtual void show(vector<Event>,vector<Site>) = 0;
+	virtual void printEvents(vector<Event*>_e) {
+		if (_e.empty())return;
+		sort(_e.begin(), _e.end(), sortByTime);
+		cout << "Events held here: \n";
+		for (int i = 0; i < _e.size(); ++i) {
+			cout <<"\t"<<_e[i]->time  << "         " <<_e[i]->eventName  <<endl;
+		}
+	}
 };
 class SortByName :public Compositor {
 public:
 	SortByName() {}
-	virtual void show(vector<Event>_events) {
-		sort(_events.begin(), _events.end(), sortByName);
-		for (int i = 0; i < _events.size(); ++i) {
-			cout << "项目名称：" << setw(16) << left << _events[i].name << "  开始时间：" << _events[i].startTime << endl;
+	virtual void show(vector<Event>_events,vector<Site>_sites) {
+		sort(_sites.begin(), _sites.end(), sortByName);
+		for (int i = 0; i < _sites.size(); ++i) {
+			cout << "Name of site：" << setw(20) << left << _sites[i].siteName << "  Site number：" << _sites[i].num << endl;
+			printEvents(_sites[i]._events);
+		}
+	}
+};
+class SortByNum :public Compositor {
+public:
+	SortByNum() {}
+	virtual void show(vector<Event>_events, vector<Site>_sites) {
+		sort(_sites.begin(), _sites.end(), sortByNum);
+		for (int i = 0; i < _sites.size(); ++i) {
+			cout << "Site number：" << _sites[i].num << "  Name of site：" << setw(20) << left << _sites[i].siteName << endl;
+			printEvents(_sites[i]._events);
 		}
 	}
 };
 class SortByTime :public Compositor {
 public:
 	SortByTime() {}
-	virtual void show(vector<Event>_events) {
-		sort(_events.begin(), _events.end(), sortByTime);
+	virtual void show(vector<Event>_events, vector<Site>_sites) {
+		sort(_events.begin(), _events.end());
 		for (int i = 0; i < _events.size(); ++i) {
-			cout << "开始时间：" << _events[i].startTime << "  项目名称：" << _events[i].name << endl;
+			cout << "Event name: " << setw(25)<<_events[i].eventName << "  time: " << _events[i].time << "  site: " << _events[i].heldSite << endl;
 		}
 	}
 };
 class Menu {
 private:
-	Compositor* _compositor;
-	vector<Event>_events;
+	Compositor* _compositor[3];
+	vector<Event>* _events;
+	vector<Site>* _sites;
 public:
-	Menu(Compositor* c, vector<Event>v) :_compositor(c), _events(v) {}
-	void Show() {
-		_compositor->show(_events);
+	Menu(vector<Event>*v,vector<Site>*s) :_events(v),_sites(s) {
+		_compositor[0] = new SortByName;
+		_compositor[1] = new SortByNum;
+		_compositor[2] = new SortByTime;
+	}
+	//使用三种不同策略对场地进行展示
+	void Show(string Mode) {
+		if(Mode=="name")
+			_compositor[0]->show(*_events, *_sites);
+		else if (Mode =="number")
+			_compositor[1]->show(*_events, *_sites);
+		else if(Mode=="time")
+			_compositor[2]->show(*_events, *_sites);
 	}
 };
