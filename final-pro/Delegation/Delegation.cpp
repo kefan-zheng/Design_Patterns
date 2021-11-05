de30ce5464b02229d2d8f0072610b68db972dd4b
awei-mj <56434116+awei-mj@users.noreply.github.com>
2021-11-05 18:00:36 +0800

    Final Update

--- final-pro/Delegation/Delegation.cpp
+++ final-pro/Delegation/Delegation.cpp
@@ -1,4 +1,5 @@
 #include "Delegation.h"
+#include <map>
 using namespace std;
 
 bool Referees::init = false;
@@ -9,6 +10,7 @@ double Referees::score()
     double res{};
     double temp[7];
     double max{0}, min{100};
+    cout << "Referees::score() delegates Referee::score()\n";
     for (int i = 0; i != 7; ++i)
     {
         temp[i] = RefereeList[i]->score();
@@ -20,27 +22,35 @@ double Referees::score()
 }
 double Diver::score() //difficulty all set 2.0
 {
-    Referees& instance = Referees::Instance();
-    for (int i = 0; i != 10; ++i)
-        total += 2.0 * instance.score();
+    static Referees& instance = Referees::Instance();
+    cout << "diver::score() delegates Referees::score()\n";
+    for (int i = 0; i != 2; ++i)
+        total += 10 * instance.score();
     return total;
 }
 
 void testDelegation() //test
 {
+    Referees::Instance();
     vector<Diver> divers;
     divers.push_back(string("QuanHongchan"));
     divers.push_back(string("WuMinxia"));
     divers.push_back(string("LinYining"));
     srand(time(nullptr));
+    map<string, double> DeleRes;
+    for (auto &diver : divers)
+    {
+        cout << diver.getName() << ":\n";
+        DeleRes[diver.getName()] = diver.score();
+    }
     cout<< "----------------------- Diving ------------------------\n"
         << left << setw(30) << "* name"
         << left << setw(24) << "score" << "*\n"
         << "-------------------------------------------------------\n";
-    for (auto diver : divers)
+    for (auto diver : DeleRes)
     {
-        cout<< "* " << left << setw(28) << diver.getName()
-            << left << setw(24) << diver.score() << "*\n";
+        cout<< "* " << left << setw(28) << diver.first
+            << left << setw(24) << diver.second << "*\n";
     }
     cout<< "-------------------------------------------------------\n";
 }
