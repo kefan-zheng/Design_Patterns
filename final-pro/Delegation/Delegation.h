f350eb768d44cb0825b0035555ec9ae0b4188dc1
awei-mj <56434116+awei-mj@users.noreply.github.com>
2021-11-05 17:59:24 +0800

    Final Update

--- final-pro/Delegation/Delegation.h
+++ final-pro/Delegation/Delegation.h
@@ -2,22 +2,23 @@
 #include <iostream>
 #include <vector>
 #include<iomanip>
-#include "../Prototype/Prototype.h"
+#include "Prototype.h"
 //take diving as example
-class Diver
+
+class Diver : public Delegation
 {
 public:
-    Diver() {}
-    Diver(std::string n) : name(n) {}
+    Diver():total(0) {}
+    Diver(std::string n) : name(n), total(0) {}
     ~Diver() {}
     double score();
-    const std::string& getName() { return name; }
+    const std::string& getName() const { return name; }
 
 private:
     double total;
     std::string name;
 };
-class Referees //Diver delegates this class
+class Referees : public Delegation //Diver delegates this class
 {
 public:
     static Referees& Instance() //return the instance
@@ -26,9 +27,14 @@ public:
         {
             init = true;
             Referee* proto = new Referee();
+            std::cout << "Prototype Referee created.\n";
             instance.RefereeList.push_back(proto);
             for (int i = 0; i != 6; ++i)
+            {
                 instance.RefereeList.push_back(proto->Clone());
+                std::cout << "Referee cloned " << i + 1 << "/6\n";
+            }
+            std::cout << "Referee creation completed! Scoring..." << std::endl;
         }
         return instance;
     }
